650cba107cbf34b79bb8e6feffd20f70ff78d858
Евгений <evg.gaidamakin@gmail.com>
2025-02-04 09:52:33 +0200

    edit

--- js/main.js
+++ js/main.js
@@ -1,4 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
+	const radioLabels = document.querySelectorAll('.input_wrap')
+
+	const button = document.querySelector('.form_home .button_wrap button ')
+	radioLabels.forEach(label => {
+		label.addEventListener('click', () => {
+			radioLabels.forEach(lbl => lbl.classList.remove('input_checked'))
+			button.classList.remove('button_active')
+
+			// Добавляем рамку выбранному
+			label.classList.add('input_checked')
+
+			// Меняем цвет кнопки
+
+			button.classList.add('button_active')
+		})
+	})
+	// function activeBtn() {
+	// 	document
+	// 		.querySelector('.form_home .button_wrap button ')
+	// 		.classList.add('button_active')
+	// }
+
 	function inputLink() {
 		const inputs = document.querySelectorAll('input')
 		console.log(inputs, 'inputs')
@@ -9,7 +31,6 @@ document.addEventListener('DOMContentLoaded', () => {
 			if (!item.checked) {
 				return
 			}
-
 			checkedInputs.push(item.value)
 		})
 
@@ -19,4 +40,14 @@ document.addEventListener('DOMContentLoaded', () => {
 	}
 
 	document.querySelector('.continue_btn').addEventListener('click', inputLink)
+
+	// document
+	// 	.getElementById('radioForm')
+	// 	.addEventListener('change', function (event) {
+	// 		if (event.target.type === 'radio') {
+	// 			window.location.href = event.target.value
+	// 		}
+	// 	})
+	// }
+	// document.querySelector('.continue_btn').addEventListener('click', inputLink)
 })
